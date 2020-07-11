e0159b5045938ae5e51f7f5d42e193cf2b58c3cf
SSwedha <38497040+SSwedha@users.noreply.github.com>
2020-07-12 00:43:49 +0530

    linear reg

--- OCT_scan.py
+++ OCT_scan.py
@@ -18,6 +18,8 @@ from skimage.segmentation import active_contour
 from skimage.filters import meijering, sato, frangi, hessian
 from scipy.ndimage.filters import uniform_filter
 from scipy.ndimage.measurements import variance
+from sklearn import datasets, linear_model, metrics 
+from sklearn.model_selection import train_test_split 
 
 def auto_canny(image, sigma = 0.7):
     # compute the median of the single channel pixel intensities
@@ -33,6 +35,21 @@ def auto_canny(image, sigma = 0.7):
 
 def nothing(x):
     pass
+
+def crop(img, i):
+    # Returns dimensions of image
+    height, width = img.shape[0:2]
+    # Top and Bottom range
+    startrow = int(height*.12)
+    endrow = int(height*.88)
+    startcol = int(width*.05)
+    endcol = int(width*0.95)
+    img = img[startrow:endrow, startcol:endcol]
+    height, width = img.shape[0:2]
+    image = img[0:height, startcol:endcol]
+    height, width = img.shape[0:2]
+    image = image[20:520, 20:520]
+    return image
 
 def write_image(path, img):
     # img = img*(2**16-1)
@@ -53,11 +70,71 @@ def lee_filter(img, size):
     img_output = img_mean + img_weights * (img - img_mean)
     return img_output
 
+def filterimage(img):
+    #cv2.imshow('i',img)
+    #croppedImage = img[startRow:endRow, startCol:endCol]
+     
+    kernel = np.ones((5,5),np.uint8)
+
+    dilation = cv2.dilate(img,kernel,iterations = 1)
+    blur = dilation
+    #blur = cv2.GaussianBlur(dilation,(5,5),0)
+    #blur = cv2.blur(dilation,(5,5))
+    #plt.imshow(blur,cmap = 'gray')
+    #plt.show()
+    #dilation = cv2.dilate(blur,kernel,iterations = 1)
+    #blur = cv2.GaussianBlur(dilation,(5,5),0)
+    #blur = cv2.GaussianBlur(im,(5,5),0)
+
+    erosion = cv2.erode(blur,kernel,iterations = 1)
+
+    #blur = cv2.blur(erosion,(10,10),0)
+    plt.imshow(blur,cmap = 'gray')
+    plt.show()
+
+    opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    gradient = cv2.morphologyEx(blur, cv2.MORPH_GRADIENT, kernel)
+    dil = cv2.erode(gradient,kernel,iterations = 1) 
+
+    plt.subplot(2,3,1),plt.imshow(img,cmap = 'gray')
+    plt.title('Original'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,3,2),plt.imshow(erosion,cmap = 'gray')
+    plt.title('Erosion'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,3,3),plt.imshow(dilation,cmap = 'gray')
+    plt.title('Dilation'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,3,4),plt.imshow(opening,cmap = 'gray')
+    plt.title('Opening'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,3,5),plt.imshow(dil,cmap = 'gray')
+    plt.title('Dil'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,3,6),plt.imshow(gradient,cmap = 'gray')
+    plt.title('Gradient'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+    laplacian = cv2.Laplacian(img,cv2.CV_64F)
+    sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+    sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+    sobel = cv2.addWeighted(np.absolute(sobelx), 0.5, np.absolute(sobely), 0.5, 0)
+    #sobel = cv2.Sobel(img,cv2.CV_64F,1,1,ksize=5)
+    sobel = cv2.erode(sobel,kernel,iterations = 1)
+
+    plt.subplot(2,2,1),plt.imshow(sobel,cmap = 'gray')
+    plt.title('Sobel'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+    plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+    plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+    plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+
+    plt.show()
+
 def threshold_tb(image, threshold = 60, th_type = 3):
     alpha = 100
     beta = 0
     s = 0
     threshold = 125
+    
     '''
     cv2.namedWindow('Contrast')
     # create trackbars for color change
@@ -90,6 +167,7 @@ def threshold_tb(image, threshold = 60, th_type = 3):
     '''
     i = cv2.convertScaleAbs(image, alpha=alpha/100, beta=-1*beta)
     _, dst = cv2.threshold(i, threshold, 255, th_type)
+    
     #cv2.imshow('Threshold', dst)
     #cv2.waitKey(1)
     
@@ -99,18 +177,17 @@ if __name__=="__main__":
     
     # Image files location 
     location1 = 'OCT_scan'
-    location1 = 'Documents\GitHub\Optic_Disk\Images\_CS'
     #location2 = 'Documents\GitHub\Optic_Disk\Images_Processed\_OD'
     # Loop through all the images
 
-    for i in range(1, 29):
+    for i in range(0, 2):
 
         image1 = location1 + str(i) + '.jpeg'    # Filename
         #image2 = location2 + str(i) + '.jpg'    # Filename
         img1 = cv2.imread(image1,0)              # Read image
         #img2 = cv2.imread(image2,0)             # Read image
         #img1 = cv2.medianBlur(img1,15)
-        #img1 = cv2.GaussianBlur(img1,(15,15),0)
+        img1 = cv2.GaussianBlur(img1,(5,5),0)
         #img1 = cv2.blur(img1,(15,15))
         #cv2.imshow('image',img1)
         img = img1.copy()
@@ -124,6 +201,14 @@ if __name__=="__main__":
 
         height, width = img.shape[0:2]
         pivot = np.zeros((width,1))
+
+        Xl = np.zeros((height*width,2))
+        wpx_l = 0
+        output_l = np.zeros((height*width,1))
+        Xr = np.zeros((height*width,2))
+        wpx_r = 0        
+        output_r = np.zeros((height*width,1))
+
         for b in range(width):
             for a in range(height):
                 if thresh[a,b] > 200:
@@ -139,98 +224,95 @@ if __name__=="__main__":
         partl = thresh.copy()
         partr = thresh.copy()
 
+        for b in range(width):
+            fl = 0
+            for a in range(height):
+                if partl[a,b] > 200:
+                    fl = fl + 1
+                if partl[a,b] < 10 and fl < 20:
+                    partl[a,b] = 255
+                if fl > 20:
+                    break
+            fl = 0
+            for a in range(height):
+                if partl[a,b] < 10:
+                    fl = fl + 1
+                if partl[a,b] > 200 and fl < 20:
+                    partl[a,b] = 0
+                if fl > 20:
+                    break
+            fl = 0
+            for a in range(height):
+                if partl[a,b] > 200 and fl < 20:
+                    fl = fl + 1
+                    continue
+                partl[a,b] = 0
+        
+        partr = partl.copy()
+
         for a in range(width):
-            if a > 1*maxpos:
+            if a > 0.6*maxpos:
                 partl[:, a] = 0
-            if a < 1*maxpos:
+            if a < 1.4*maxpos:
                 partr[:, a] = 0
 
-        for b in range(int(0.25*width)):
-            for a in range(int(0.75*height),height,1):
-                partl[a,b] = 0
+        for b in range(width):
+            for a in range(height):
+                if partl[a,b] > 200:
+                    Xl[wpx_l,0] = float(a/height)
+                    Xl[wpx_l,1] = float(b/width)
+                    output_l[wpx_l] = float(partl[a,b]/255)
+                    wpx_l = wpx_l + 1
+                    
+                if partr[a,b] > 200:
+                    Xr[wpx_r,0] = float(a/height)
+                    Xr[wpx_r,1] = float(b/width)
+                    output_r[wpx_r] = float(partr[a,b]/255)
+                    wpx_r = wpx_r + 1
 
-        #cv2.imshow('partl',partl)
-        #cv2.waitKey(1)
+        print(wpx_l,wpx_r)
         
-        partl_top = np.zeros((height,width))
-        partr_top = np.zeros((height,width))
+        Xl = np.delete(Xl, slice(wpx_l,height*width), 0)
+        Xr = np.delete(Xr, slice(wpx_r,height*width), 0)
+        output_l = np.delete(output_l,slice(wpx_l,height*width),0)
+        output_r = np.delete(output_r,slice(wpx_r,height*width),0)
 
-        partl_bottom = np.zeros((height,width))
-        partr_bottom = np.zeros((height,width))
-
-        partl_top = partl.copy()
-        partr_top = partr.copy()
-
-        partl_bottom = partl.copy()
-        partr_bottom = partr.copy()
-
-        for b in range(width):
-            fl = 0
-            for a in range(height):
-                if partl_top[a,b] > 200 and fl < 20:
-                    fl = fl + 1
-                    continue
-                
-                if fl < 20 and b < int(0.4*width):
-                    partl_top[a,b] = 255
-
-        for b in range(width):
-            fl = 0
-            for a in range(height):
-                if partr_top[a,b] > 200 and fl < 20:
-                    fl = fl + 1
-                    continue
-                
-                if fl < 20 and b > int(0.65*width):
-                    partr_top[a,b] = 255
+        print(Xl.shape[0:2])
+        print(Xr.shape[0:2])
 
 
-        for b in range(width):
-            fl = 0
-            for a in range(height-1,0,-1): #thin line of layer
-                if partl_bottom[a,b] > 200 and fl < 15:
-                    fl = fl + 1
-                    continue
-                if fl>= 15:
-                    partl_bottom[a,b] = 0
-                if fl < 15 and b < int(0.4*width):
-                    partl_bottom[a,b] = 255
-
-        for b in range(width):
-            fl = 0
-            for a in range(height-1,0,-1): #thin line of layer
-                if partr_bottom[a,b] > 200 and fl < 15:
-                    fl = fl + 1
-                    continue
-                if fl>= 15:
-                    partr_bottom[a,b] = 0
-                if fl < 15 and b > int(0.65*width):
-                    partr_bottom[a,b] = 255
-
-        for b in range(width):
-            for a in range(height):
-                if partl_bottom[a,b] > 200 or partl_top[a,b] > 200:
-                    partl[a,b] = 255
-                if partr_bottom[a,b] > 200 or partr_top[a,b] > 200:
-                    partr[a,b] = 255
-                
-             
-        plt.subplot(2,3,2),plt.imshow(partl,cmap = 'gray')
-        plt.title('Partl'), plt.xticks([]), plt.yticks([])
-        plt.subplot(2,3,3),plt.imshow(partr,cmap = 'gray')
-        plt.title('Partr'), plt.xticks([]), plt.yticks([])      
+        Xl_train, Xl_test, yl_train, yl_test = train_test_split(Xl, output_l, test_size=0.4, random_state=1) 
+        Xr_train, Xr_test, yr_train, yr_test = train_test_split(Xr, output_r, test_size=0.4, random_state=1) 
+        
+        reg = linear_model.LinearRegression() 
+        # train the model using the training sets 
+        reg.fit(Xl_train, yl_train) 
+        # regression coefficients 
+        print('Coefficients: \n', reg.coef_) 
+  
+        # variance score: 1 means perfect prediction 
+        print('Variance score: {}'.format(reg.score(Xl_test, yl_test))) 
 
         picture = partl + partr
 
-        for b in range(int(0.4*width),int(0.7*width),1):
-            for a in range(height):
-                picture[a,b] = 255
+
+        cv2.imshow('picture', picture)
+
         
-        picture = np.invert(picture)
+        '''
+        kernel = np.ones((5,5),np.uint8)
+        erosion = cv2.erode(sobely,kernel,iterations = 2)
+        dilation = cv2.dilate(erosion,kernel,iterations = 3)
+        opening = cv2.morphologyEx(sobely, cv2.MORPH_OPEN, kernel)
+        #closing = cv2.morphologyEx(picture, cv2.MORPH_CLOSE, kernel)
+        cv2.imshow('ero-dil', dilation)
+        #cv2.imshow('ero', erosion)
+        cv2.imshow('ope', opening)
+        #cv2.imshow('clo', closing)
+        cv2.waitKey(0) 
+        '''
+
         
-        plt.subplot(2,3,4),plt.imshow(picture,cmap = 'gray')
-        plt.title('Combined_image'), plt.xticks([]), plt.yticks([])
-        plt.show()
 
         '''
         contour1,hierarchy1 = cv2.findContours(partl, 1, 2)
@@ -259,6 +341,8 @@ if __name__=="__main__":
         
         cv2.waitKey(0)
         '''
+        
+        
         #threshold_tb(image)
 
         #path = 'Documents\GitHub\Optic_Dsk\Images_Processed\_CS' + str(i) + '.jpeg'
@@ -271,7 +355,7 @@ if __name__=="__main__":
         #equ = cv2.equalizeHist(image)
         #equ = image - image2
         
-        """
+        '''
         for i in range(height):
             for j in range(width):
                 if (mask[i,j] != 0 and mask[i,j] != 255):
@@ -285,9 +369,9 @@ if __name__=="__main__":
         plt.subplot(1,3,3),plt.imshow(final,cmap = 'gray')
         plt.title('Final'), plt.xticks([]), plt.yticks([])
         plt.show()
-        """
+        '''
         
-        """
+        ''''
         crim = cl2
         unsharp = Image.fromarray(crim.astype('uint8'))
         unsharp_class = unsharp.filter(ImageFilter.UnsharpMask(radius=2, percent=150))
@@ -303,7 +387,7 @@ if __name__=="__main__":
         noisy = np.uint8(np.clip(img,0,255))
 
         dst_bw = cv2.fastNlMeansDenoisingMulti(noisy, 2, 5, None, 4, 7, 35)
-        """
+        '''
     
         cv2.waitKey(0)
     cv2.destroyAllWindows
