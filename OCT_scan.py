0bdf8d72c22197de62789b8e433870924fe8637b
SSwedha <38497040+SSwedha@users.noreply.github.com>
2020-07-12 19:37:21 +0530

    linear reg, updated

--- OCT_scan.py
+++ OCT_scan.py
@@ -173,6 +173,33 @@ def threshold_tb(image, threshold = 60, th_type = 3):
     
     return dst
 
+def linear_regression(Xl,Xr):
+    nl = Xl.shape[0]
+    nr = Xr.shape[0]
+  
+    # mean of x and y vector 
+    m_xl, m_yl = np.mean(Xl[:,0]), np.mean(Xl[:,1]) 
+    m_xr, m_yr = np.mean(Xr[:,0]), np.mean(Xr[:,1]) 
+  
+    # calculating cross-deviation and deviation about Xl and Xr
+    SS_xy_l = np.sum(Xl[:,1]*Xl[:,0]) - nl*m_yl*m_xl 
+    SS_xx_l = np.sum(Xl[:,0]*Xl[:,0]) - nl*m_xl*m_xl 
+    
+    SS_xy_r = np.sum(Xr[:,1]*Xr[:,0]) - nr*m_yr*m_xr 
+    SS_xx_r = np.sum(Xr[:,0]*Xr[:,0]) - nr*m_xr*m_xr 
+
+    # calculating regression coefficients 
+    bl = np.zeros((2))
+    br = np.zeros((2))
+    bl[1] = SS_xy_l / SS_xx_l
+    bl[0] = m_yl - bl[1]*m_xl
+    
+    br[1] = SS_xy_r / SS_xx_r
+    br[0] = m_yr - br[1]*m_xr
+
+    return bl, br
+
+
 if __name__=="__main__":
     
     # Image files location 
@@ -180,7 +207,7 @@ if __name__=="__main__":
     #location2 = 'Documents\GitHub\Optic_Disk\Images_Processed\_OD'
     # Loop through all the images
 
-    for i in range(0, 2):
+    for i in range(24, 29):
 
         image1 = location1 + str(i) + '.jpeg'    # Filename
         #image2 = location2 + str(i) + '.jpg'    # Filename
@@ -204,10 +231,10 @@ if __name__=="__main__":
 
         Xl = np.zeros((height*width,2))
         wpx_l = 0
-        output_l = np.zeros((height*width,1))
+        
         Xr = np.zeros((height*width,2))
         wpx_r = 0        
-        output_r = np.zeros((height*width,1))
+        
 
         for b in range(width):
             for a in range(height):
@@ -255,50 +282,98 @@ if __name__=="__main__":
                 partl[:, a] = 0
             if a < 1.4*maxpos:
                 partr[:, a] = 0
-
+        partl_og = partl.copy()
+        partr_og = partr.copy()
+        
+        kernel = np.ones((5,5),np.uint8)
+        erosion_l = cv2.erode(partl,kernel,iterations = 1)
+        dilation_l = cv2.dilate(erosion_l,kernel,iterations = 2)
+        partl = dilation_l
+        
+        erosion_r = cv2.erode(partr,kernel,iterations = 1)
+        dilation_r = cv2.dilate(erosion_r,kernel,iterations = 2)
+        partr = dilation_r
+        
         for b in range(width):
             for a in range(height):
                 if partl[a,b] > 200:
-                    Xl[wpx_l,0] = float(a/height)
-                    Xl[wpx_l,1] = float(b/width)
-                    output_l[wpx_l] = float(partl[a,b]/255)
-                    wpx_l = wpx_l + 1
+                    Xl[wpx_l,0] = a  #column 0 has x coord of pixels which are white
+                    Xl[wpx_l,1] = b  #column 1 has y coord of pixels which are white
+                    wpx_l = wpx_l + 1 #counter to know the index of row
                     
                 if partr[a,b] > 200:
-                    Xr[wpx_r,0] = float(a/height)
-                    Xr[wpx_r,1] = float(b/width)
-                    output_r[wpx_r] = float(partr[a,b]/255)
+                    Xr[wpx_r,0] = a
+                    Xr[wpx_r,1] = b
                     wpx_r = wpx_r + 1
 
         print(wpx_l,wpx_r)
-        
-        Xl = np.delete(Xl, slice(wpx_l,height*width), 0)
-        Xr = np.delete(Xr, slice(wpx_r,height*width), 0)
-        output_l = np.delete(output_l,slice(wpx_l,height*width),0)
-        output_r = np.delete(output_r,slice(wpx_r,height*width),0)
 
+        Xl = np.delete(Xl, slice(wpx_l,height*width), 0) #to delete the unnecessary rows
+        Xr = np.delete(Xr, slice(wpx_r,height*width), 0)
+        
         print(Xl.shape[0:2])
         print(Xr.shape[0:2])
 
-
-        Xl_train, Xl_test, yl_train, yl_test = train_test_split(Xl, output_l, test_size=0.4, random_state=1) 
-        Xr_train, Xr_test, yr_train, yr_test = train_test_split(Xr, output_r, test_size=0.4, random_state=1) 
+        Xl_train, Xl_test, yl_train, yl_test = train_test_split(Xl[:,1].reshape((-1,1)), Xl[:,0], test_size=0.2, 
+                                                    random_state=1) 
         
-        reg = linear_model.LinearRegression() 
+        Xr_train, Xr_test, yr_train, yr_test = train_test_split(Xr[:,1].reshape((-1,1)), Xr[:,0], test_size=0.2, 
+                                                    random_state=1) 
+
+        # create linear regression object 
+        reg_l = linear_model.LinearRegression() 
+        
         # train the model using the training sets 
-        reg.fit(Xl_train, yl_train) 
-        # regression coefficients 
-        print('Coefficients: \n', reg.coef_) 
-  
-        # variance score: 1 means perfect prediction 
-        print('Variance score: {}'.format(reg.score(Xl_test, yl_test))) 
+        reg_l.fit(Xl_train, yl_train) 
 
-        picture = partl + partr
-
-
-        cv2.imshow('picture', picture)
-
+        ml = reg_l.coef_[0]
+        cl = reg_l.intercept_
         
+        yl_1 = cl + ml*1
+        yl_2 = cl + ml*(width-1)
+
+         # create linear regression object 
+        reg_r = linear_model.LinearRegression() 
+        
+        # train the model using the training sets 
+        reg_r.fit(Xr_train, yr_train) 
+
+        mr = reg_r.coef_[0]
+        cr = reg_r.intercept_
+        
+        yr_1 = cr + mr*1
+        yr_2 = cr + mr*(width-1)
+
+        picture = partl_og + partr_og
+        picture_color = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
+        picture_l = cv2.line(picture_color, (1,int(yl_1)),(int(width-1),int(yl_2)), (0,255,0),3)
+        picture_r = cv2.line(picture_l, (1,int(yr_1)),(int(width-1),int(yr_2)), (0,0,255),3)
+        theta = math.degrees(math.atan(ml)-math.atan(mr))
+        print('theta: ',theta)
+        cv2.imshow('pic'+ str(i),picture_r)      
+
+       
+        '''        
+        bl, br =linear_regression(Xl,Xr) 
+        print(bl, br)
+
+        yl_pred = np.zeros(2)
+        yr_pred = np.zeros(2)
+
+        yl_pred[0] = bl[0] + bl[1]*1
+        int(width/2) = bl[0] + bl[1]*x
+
+        yr_pred[0] = br[0] + bl[1]*int(width/2)
+        yr_pred[1] = br[0] + bl[1]*(width-1)
+        
+        picture = partl + partr
+        picture1 = cv2.line(picture,(1,int(yl_pred[0])),(int(width/2),int(yl_pred[1])),(0,255,0),3)
+        picture2 = cv2.line(picture1,(int(width/2),int(yr_pred[0])),((width-1),int(yr_pred[1])),(0,255,0),3)
+        
+        cv2.imshow('picture with lines', picture2)
+
+        print(partl.shape[0:2],partr.shape[0:2])
+        '''
         '''
         kernel = np.ones((5,5),np.uint8)
         erosion = cv2.erode(sobely,kernel,iterations = 2)
